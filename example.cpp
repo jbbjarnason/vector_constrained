cc802762a951726bb27cacc554aa91ef18cacbd0
jbbjarnason <jon.bjarni@menandmice.com>
2021-12-02 17:10:02 +0000

    example a bit more thorough

--- example.cpp
+++ example.cpp
@@ -1,28 +1,45 @@
-import <cstdio>;
-import <memory>;
-
 import nonstd.vector_constrained;
+import<cassert>;
+import<cstdio>;
+import<exception>;
 
 struct constrain_less_than_10 {
-    bool operator() (int value) {
+    bool operator()(int value) {
         return value < 10;
     }
 };
+using lessthan10 = nonstd::vector_constrained<int, std::allocator<int>, constrain_less_than_10>;
 
 struct constrain_less_than_20 {
-    bool operator() (int value) {
-        return value < 10;
+    bool operator()(int value) {
+        return value < 20;
     }
 };
+using lessthan20 = nonstd::vector_constrained<int, std::allocator<int>, constrain_less_than_20>;
 
 int main() {
-    auto vec{ nonstd::vector_constrained<int, std::allocator<int>, constrain_less_than_10>() };
-    vec.assign({1,2,3,4,5,6,7,8,9});
-    auto vec2{ nonstd::vector_constrained<int, std::allocator<int>, constrain_less_than_20>() };
-    vec2.assign({1,2,3,4,5,6,7});
-    if (vec == vec2) {
-        printf("wtf\n");
+    lessthan10 first_vector{{1, 2, 3, 4, 5, 6, 7, 8, 9}};
+    lessthan20 second_vector{first_vector};// copy the first_vector
+    assert(("Oboj something is wrong", first_vector == second_vector));
+
+    second_vector.assign({10, 11});// overwrite vector values
+    second_vector.insert(second_vector.end(), {12, 13});
+    second_vector.push_back(14);
+    printf("14 is at 4, see: %d\n", second_vector[4]);// out: 14 is at 4, see: 14
+
+    try {
+        first_vector.push_back(10);// invalid value ...
+    } catch (const std::exception &ex) {
+        printf("Catched it: %s\n", ex.what());// out: Catched it: Argument does not fulfill the predicate criteria
     }
-    printf("size is %d\n", vec.size());
+
+    std::vector<int> normal_vector{10, 11, 12, 13, 14};
+    assert(("constrained vec can be compared with regular", second_vector == normal_vector));
+
+    lessthan20 third_vector{normal_vector};// construct from std::vector
+    assert(("still the same", third_vector == normal_vector));
+
+    // ... you get the point
+
     return 0;
 }
